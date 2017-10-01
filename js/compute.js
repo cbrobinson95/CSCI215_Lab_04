c9256297cdea66e93cecfd66e1333ce682caec69
cbrobinson95 <cbrobinson95@Christophers-MacBook-Pro.local>
2017-10-01 13:12:28 -0400

    Final Commit

--- js/compute.js
+++ js/compute.js
@@ -1,37 +1,55 @@
+console.clear();
+
+// -----------------------------------
+// Global Variables
+
 var I = [];
 
-I[0] = [];
-I[0][0] = “Array Position 0”;
-I[0][1] = “Array Position 1”;
-I[0][2] = “Array Position 2”;
-I[0][3] = “Array Position 3”;
-I[0][4] = “Array Position 4”;
+I[0]='http://munsellb.people.cofc.edu/img/pound.jpeg';
+I[1]='http://munsellb.people.cofc.edu/img/euro.jpeg';
+I[2]='http://munsellb.people.cofc.edu/img/kroon.jpeg';
+I[3]='http://munsellb.people.cofc.edu/img/canadian.jpeg';
+I[4]='http://munsellb.people.cofc.edu/img/yuan.jpeg';
 
-I[1] = [];
-I[1][0] = 'http://munsellb.people.cofc.edu/img/pound.jpeg’;
-I[1][1] = 'http://munsellb.people.cofc.edu/img/euro.jpeg’;
-I[1][2] = 'http://munsellb.people.cofc.edu/img/kroon.jpeg’;
-I[1][3] = 'http://munsellb.people.cofc.edu/img/canadian.jpeg’;
-I[1][4] = 'http://munsellb.people.cofc.edu/img/canadian.jpeg’;
+// -----------------------------------
+// Convert Function Goes Here
 
-document.write(I[0][0] + “I[1][0]”);
-document.write(I[0][1] + “I[1][1]”);
-document.write(I[0][2] + “I[1][2]”);
-document.write(I[0][3] + “I[1][3]”);
-document.write(I[0][4] + “I[1][4]”);
+function convert() {
 
-convert() {
-    var dollar = document.getElementsbyName(“dollars”)[0].value;
-    var selectElement = document.getElementbyId(‘currency’);
+    var dollar = document.getElementsByName("dollars")[0].value;
+    var selectElement = document.getElementById('currency');
     var exchangeRate = selectElement.value;
     var index = selectElement.selectedIndex;
     var currency = selectElement.options[index].text;
+
+    updateExchange(dollar, exchangeRate, currency, index);
+
 }
 
-updateExchange(dollar, exchangeRate, currency, index); {
+// -----------------------------------
+// Calculate Function Goes Here
+
+function calculate(dollar, exchangeRate) {
+
+    return dollar*exchangeRate;
 
 }
 
-calculate(dollar, exchangeRate); {
-    return dollar*echangeRate
+// -----------------------------------
+// Functions that are given
+// Do not modify
+
+function updateImage( index ) {
+
+    var url = 'url(' + I[index] + ')';
+    document.getElementById('bill_img').style.backgroundImage=url;
+
+}
+
+function updateExchange(dollar, exchangeRate, currency, index) {
+
+    document.getElementsByName("exchange")[0].value = calculate( dollar, exchangeRate ).toPrecision(4) + ' ' + currency;
+
+    updateImage(index);
+
 }
